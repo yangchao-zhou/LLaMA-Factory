84acd2207720325173bad786a7e858740a7fd74a
yangchao.zhou <yangchao.zhou@kunlun-inc.com>
2025-02-28 03:59:13 +0800

    终于前端可以了

--- evaluation/gradio_vllm.py
+++ evaluation/gradio_vllm.py
@@ -1,15 +1,11 @@
 from openai import OpenAI
 import gradio as gr
-import re
-'''
-vllm serve /maindata/data/shared/public/yangchao.zhou/projects/LLaMA-Factory/saves/mistral-24b-linky/full/sft-20250226-tulu_deepseek_score_role_play/checkpoint-2300 --tensor-parallel-size 1 --port 8000
 
-'''
 # Set OpenAI's API key and API base to use vLLM's API server.
 openai_api_key = "EMPTY"
 # openai_api_base = "http://localhost:8000/v1"
 openai_api_base = "http://172.20.208.101:8000/v1"
-model="/maindata/data/shared/public/yangchao.zhou/projects/LLaMA-Factory/saves/mistral-24b-linky/full/sft-20250226-tulu_deepseek_score_role_play/checkpoint-2500"
+model="/maindata/data/shared/public/yangchao.zhou/projects/LLaMA-Factory/saves/mistral-24b-linky/full/sft-20250226-tulu_deepseek_score_role_play/checkpoint-1700"
 
 
 # 创建一个 OpenAI 客户端，用于与 API 服务器进行交互
@@ -17,8 +13,7 @@ client = OpenAI(
     api_key=openai_api_key,
     base_url=openai_api_base,
 )
-
-system = "你一个昆仑万维公司开发的模型"
+system = '''请用长思维链的方式回答这个问题。长思维链的内容在<think>和</think>之间。'''
 def predict(message, history):
     # 将聊天历史转换为 OpenAI 格式
     history_openai_format = [{"role": "system", "content": system}]
@@ -26,15 +21,13 @@ def predict(message, history):
         history_openai_format.append({"role": "user", "content": human })
         history_openai_format.append({"role": "assistant", "content":assistant})
     history_openai_format.append({"role": "user", "content": message})
-    
 
     # 创建一个聊天完成请求，并将其发送到 API 服务器
     stream = client.chat.completions.create(
         model=model,   # 使用的模型名称
         messages= history_openai_format,  # 聊天历史
-        temperature=0.15,                  # 控制生成文本的随机性
+        temperature=0.8,                  # 控制生成文本的随机性
         stream=True,                      # 是否以流的形式接收响应
-        max_tokens=8192
         # extra_body={
         #     'repetition_penalty': 1, 
         #     'stop_token_ids': [7]
---
+++ evaluation/gradio_vllm_chat.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+import gradio as gr
+
+# Set OpenAI's API key and API base to use vLLM's API server.
+openai_api_key = "EMPTY"
+# openai_api_base = "http://localhost:8000/v1"
+openai_api_base = "http://172.20.208.101:8000/v1"
+model="/maindata/data/shared/public/yangchao.zhou/projects/LLaMA-Factory/saves/mistral-24b-linky/full/sft-20250226-tulu_deepseek_score_role_play/checkpoint-2500"
+
+
+# 创建一个 OpenAI 客户端，用于与 API 服务器进行交互
+client = OpenAI(
+    api_key=openai_api_key,
+    base_url=openai_api_base,
+)
+system = '''请用长思维链的方式回答。回答之前先思考一下，长思维链的内容在<think>和</think>之间。思维链之外的内容才是真的回答'''
+def predict(message, history):
+    # 将聊天历史转换为 OpenAI 格式
+    history_openai_format = [{"role": "system", "content": system}]
+    for human, assistant in history:
+        history_openai_format.append({"role": "user", "content": human })
+        history_openai_format.append({"role": "assistant", "content":assistant})
+    history_openai_format.append({"role": "user", "content": message})
+
+    # 创建一个聊天完成请求，并将其发送到 API 服务器
+    stream = client.chat.completions.create(
+        model=model,   # 使用的模型名称
+        messages= history_openai_format,  # 聊天历史
+        temperature=0.8,                  # 控制生成文本的随机性
+        stream=True,                      # 是否以流的形式接收响应
+        # extra_body={
+        #     'repetition_penalty': 1, 
+        #     'stop_token_ids': [7]
+        # }
+    )
+
+    # 从响应流中读取并返回生成的文本
+    partial_message = ""
+    for chunk in stream:
+        partial_message += (chunk.choices[0].delta.content or "")
+        # 使用 HTML 转义以展示 <think> 和 </think> 的内容
+        yield partial_message.replace("<think>", "&lt;think&gt;").replace("</think>", "&lt;/think&gt;")
+
+# 创建一个聊天界面，并启动它，share=True 让 gradio 为我们提供一个 debug 用的域名
+gr.ChatInterface(predict).queue().launch(share=True)
